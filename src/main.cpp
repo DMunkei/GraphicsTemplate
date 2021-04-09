2e4cd3066e013a8d4464eae42553df6010adeb0f
DarkMode <I.Eisenstadt@gmx.de>
2021-04-09 18:46:31 +0200

    add core libraries, a basic main file and clang-format

---
+++ src/main.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+int main()
+{
+	if (!glfwInit())
+	{
+		std::cerr << "GLFW init failed\n";
+		return -1;
+	}
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+	GLFWwindow *window = glfwCreateWindow(1280, 720, "My App Title", nullptr, nullptr);
+	if (!window)
+	{
+		std::cerr << "Window creation failed\n";
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+	if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress))
+	{
+		std::cerr << "Failed to initialize OpenGL context\n";
+		return -1;
+	}
+	std::cout << "OpenGL Version " << GLVersion.major << "." << GLVersion.minor;
+
+	glfwSetKeyCallback(window,
+		[](GLFWwindow *window, int key, int scancode, int action, int mods)
+		{
+			// Keyboard Input
+		});
+	glfwSetMouseButtonCallback(window,
+		[](GLFWwindow *window, int button, int action, int mods)
+		{
+			// Mouse Input
+		});
+	glfwSetScrollCallback(window,
+		[](GLFWwindow *window, double xoffset, double yoffset)
+		{
+			// Scroll Wheel Input
+		});
+	glfwSetCursorPosCallback(window,
+		[](GLFWwindow *window, double xpos, double ypos)
+		{
+			// Mouse Motion Input
+		});
+
+	// Init here
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		// Draw here
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
+}
